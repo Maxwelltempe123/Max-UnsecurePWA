e26057f07fb88bd65e103466984a3f9414634dd7
Maxwelltempe123 <158523723+Maxwelltempe123@users.noreply.github.com>
2024-11-25 01:22:21 +0000

    password thing done

--- main.py
+++ main.py
@@ -24,6 +24,16 @@ def addFeedback():
         dbHandler.listFeedback()
         return render_template("/success.html", state=True, value="Back")
 
+def make_web_safe(input_string: str) -> str:
+    to_replace = ["<", ">", ";"]
+    replacements = ["%3C", "%3E", "%3B"]
+    char_list = list(input_string)
+    for i in range(len(char_list)):
+        if char_list[i] in to_replace:
+            index = to_replace.index(char_list[i])
+            char_list[i] = replacements[index]
+
+
 
 @app.route("/signup.html", methods=["POST", "GET", "PUT", "PATCH", "DELETE"])
 def signup():
@@ -35,9 +45,11 @@ def signup():
         password = request.form["password"]
         DoB = request.form["dob"]
         dbHandler.insertUser(username, password, DoB)
+        if simple_check_password:
+            return True
         return render_template("/index.html")
     else:
-        return render_template("/signup.html")
+        return render_template("/signup.html"), print("INVALID PASSWORD")
 
 
 @app.route("/index.html", methods=["POST", "GET", "PUT", "PATCH", "DELETE"])
